46214173a68b39b6452343e2ca221402588d38d7
sol <sol@DESKTOP-SJGFTGE>
2022-01-13 20:47:23 +0900

    0113

---
+++ src/BAEKJOON/Q_2739.java
@@ -0,0 +1,19 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class Q_2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 1; i<10; i++) {
+			System.out.println(N + " * " + i + " = " + (N*i));
+		}
+		
+
+	}
+
+}
