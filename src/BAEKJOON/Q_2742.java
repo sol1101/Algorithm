a21bbf0310fce0a3741458723e8f2cd583ae0a10
sol <sol11_01@naver.com>
2022-01-19 01:11:43 +0900

    0119

---
+++ src/BAEKJOON/Q_2742.java
@@ -0,0 +1,17 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class Q_2742 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		for (int i = N; i >= 1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
